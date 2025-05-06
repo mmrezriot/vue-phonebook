29e29a892466b3f77baef32a28feaa3c0701efeb
mmrez <mohammdreza.riot7676@gmail.com>
2025-05-06 19:48:48 +0330

    add table contacts & add contact in contacts

--- app.js
+++ app.js
@@ -2,12 +2,28 @@ const app = Vue.createApp({
     data() {
         return {
             activeDisplay : 'all',
+            contacts : [
+
+            ],
+            contact :{
+                name : '',
+                phone :'',
+                email:''
+            }
 
         }
     },
     methods: {
         clickDisplay(display){
         this.activeDisplay = display
+        },
+
+        addContact(){
+           this.contacts.push(this.contact);
+           this.activeDisplay='all';
+           this.contact= {};
+
+
         }
         
 
